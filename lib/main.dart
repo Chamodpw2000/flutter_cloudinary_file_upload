4863546baa48bf8816b91986f3a6c6e2d99a4536
Chamodpw2000 <chamodpwanigasekara2000@gmail.com>
2024-12-12 07:37:12 +0530

    firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_cloudinary_file_upload/views/Home.dart';
 import 'package:flutter_cloudinary_file_upload/views/UploadArea.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
